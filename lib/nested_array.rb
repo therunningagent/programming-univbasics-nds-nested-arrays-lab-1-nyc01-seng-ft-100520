9f14bc4cfa030dc589347e36c8b2b883b9cb7451
Luis Torres <luistjr@gmail.com>
2020-09-28 20:17:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -45,10 +45,7 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
   
-   new_produce = [ 
-    
-    ["Grapefruit", "Pineapple", "Oranges","Watermelon","Eggplant"].sort,
-    ["Strawberries","Potatoes","Grapes","Avocadoes","Asparagus"].sort]
+   matrix[row][column]
     
 end
 
@@ -59,4 +56,5 @@ def matrix_update(matrix, row, column, new_value)
   
   matrix[row][column] = new_value
   matrix
+
 end 
